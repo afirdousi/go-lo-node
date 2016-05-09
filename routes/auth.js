206a31cf32ebf51cc817aa723884653b087e6d98
Anas R. Firdousi <anas.firdousi@gmail.com>
2016-05-09 12:44:40 -0700

    Added OAuth layer

--- routes/auth.js
+++ routes/auth.js
@@ -26,4 +26,61 @@ router.post("/login", function(req, res, next) {  //GET  localhost:3000/auth/log
 
 });
 
+
+//var oauthserver = require('oauth2-server');
+//var appUserHelper = require('../service/appUserHelper');
+//var ldapLookup;
+//
+//var model = webSettings.testMode ?
+//    require('../services/testModeUserModel')(webSetting) :
+//
+//app.oauth = oauthserver({
+//    model: model,
+//    grants: ['password'],
+//    debug: false
+//});
+//app.all(webSettings.contextRoot + '/oauth/token', app.oauth.grant());
+//
+//if(webSettings.testMode) {
+//
+//    app.use(webSettings.contextRoot + "/api/is-server-in-test-mode", function(req, res) {
+//        res.json({
+//            isTestMode: true
+//        });
+//    });
+//
+//    app.get(webSettings.contextRoot + "/auth/user", app.oauth.authorise(), function(req, res) {
+//        res.json([
+//            {'Name': 'Some User', 'Id': 'SomeUserId'},
+//            {'Name': 'Some Other User', 'Id': 'SomeOtherUserId'},
+//            {'Name': 'Yet Another User', 'Id': 'YetAnotherUserId'}
+//        ]);
+//    });
+//
+//}
+//else {
+//
+//    ldapLookup = require('../services/ldapLookup')(webSettings);
+//
+//
+//    app.get(webSettings.contextRoot + "/auth/user", app.oauth.authorise(), function(req, res, next) {
+//
+//        var appUser = appUserHelper(req);
+//        if (!appUser.isInAdminRole) {
+//            res.sendStatus(403);
+//        }
+//        else {
+//            ldapLookup
+//                .searchUsers(req.query.LDAPName)
+//                .then(function (result) {
+//                    res.json(result);
+//                })
+//                .catch(function (err) {
+//                    next(err);
+//                });
+//        }
+//    });
+//}
+
+
 module.exports = router;
